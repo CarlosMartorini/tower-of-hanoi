beb670da16f312cfc8431f8d78d70e175bfab895
Guilherme Rodrigues <g.srodrigues@live.com>
2021-03-30 15:03:55 -0300

    Criação das torres e inicio da estilização

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 /* CRIAÇÃO DAS TORRES */
-
-
+function torreUm(){
+    let novaTorre = document.createElement("div");
+    novaTorre.setAttribute("id", "torreUm");
+    let destino = document.getElementById("torres");
+    destino.appendChild(novaTorre);
+}
+torreUm()
+function torreDois(){
+    let novaTorre = document.createElement("div")
+    novaTorre.setAttribute("id", "torreDois");
+    let destino = document.getElementById("torres");
+    destino.appendChild(novaTorre);
+}
+torreDois()
+function torreTres(){
+    let novaTorre = document.createElement("div")
+    novaTorre.setAttribute("id", "torreTres");
+    let destino = document.getElementById("torres");
+    destino.appendChild(novaTorre);
+}
+torreTres()
 /* CRIAÇÃO DAS TORRES */
 
 
