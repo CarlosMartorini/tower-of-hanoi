f61bf5f10768ff2f18b153c07585d1e4f516b793
Carlos Martorini <c7.silveira@gmail.com>
2021-03-31 11:13:42 -0300

    Add feature select tower

--- script.js
+++ script.js
@@ -33,29 +33,58 @@ addDisc('disc4', '#329237');
 /* SELEÇÃO DAS TORRES */
 
 let selectDestinyTower = false;
+let counter = 1;
 
 const tower = document.querySelectorAll("#torres");
 
 const selectTower = (event) => {
-    console.log(event.target);
+    console.log(event.currentTarget, 
+                event.currentTarget.childElementCount, 
+                event.currentTarget.lastElementChild,
+                event.currentTarget.lastElementChild.clientWidth,
+                counter, 
+                selectDestinyTower
+                );
+
+    if (counter === 1) {
+        counter++;
+    } else if (counter === 2) {
+        counter = 1;
+        selectDestinyTower = false;
+    }
+
+    if (counter === 2) {
+        selectDestinyTower = true;
+    }
 }
 
 for (let i = 0; i < tower.length; i++) {
     tower[i].addEventListener("click", selectTower);
 }
 
-
 /* SELEÇÃO DAS TORRES */
 
 
 /* LÓGICA DE JOGO / VERIFICAÇÕES */
 
+if (selectDestinyTower === true) {
+    //se estiver na torre destino para ser selecionado
+    //quando selecionado a torre
+    //o ultimo filho deve trocar de elemento pai com appendChild
+}
+
+//quando selecionado a torre destino
+//se o tamanho do ultimo elemento filho for menor que o ultimo elemento na torre destino
+//não efetuar a troca
+//se não efetuar a troca
 
 /* LÓGICA DE JOGO / VERIFICAÇÕES */
 
 
 /* CONDIÇÃO DE VITÓRIA */
 
+//se a torreTres estiver com quatro filhos 
+//jogador venceu
 
 /* CONDIÇÃO DE VITÓRIA */
 
